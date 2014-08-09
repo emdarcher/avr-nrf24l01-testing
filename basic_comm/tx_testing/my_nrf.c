8d17dfb64029bc664a69635255ed59f82d40259d
emdarcher <emd519@yahoo.com>
2014-08-08 23:03:23 -0700

    mostly done with library, haven't tested stuff yet

--- basic_comm/tx_testing/my_nrf.c
+++ basic_comm/tx_testing/my_nrf.c
@@ -103,6 +103,10 @@ uint8_t *rw_nrf(uint8_t ReadWrite, uint8_t reg, uint8_t *val, uint8_t antVal){
 }
 void init_nrf(void){
     
+    #if USING_INT0_IRQ==1
+    init_nrf_INT0_IRQ();
+    #endif
+    
     _delay_ms(100); //allow to reach power down if shut down
     
     uint8_t val[5]; //array of ints to send to *rw_nrf function
@@ -110,32 +114,32 @@ void init_nrf(void){
     //enable auto-ack, EN_AA
     //only works with identical addresses on transmitter and receiver
     //WRITE_BIT (write mode_, EN_AA reg, val 1, 1= len of data ints
-    write_read_byte_nrf_SPI(WRITE_BIT,EN_AA, 0x01, 1); 
+    rw_nrf(WRITE_BIT,EN_AA, 0x01, 1); 
     
     //setup num of retries and delay
     //0b0010 1111 "2" sets 750uS delay between retries,
     //"F" is num retries, (1-15 now 15)
-    write_read_byte_nrf_SPI(WRITE_BIT,SETUP_RETR, 0x2F, 1);
+    rw_nrf(WRITE_BIT,SETUP_RETR, 0x2F, 1);
     
     //choose num of data pipes (1-5)
-    write_read_byte_nrf_SPI(WRITE_BIT, EN_RXADDR, 0x01, 1); //EN pipe #0
+    rw_nrf(WRITE_BIT, EN_RXADDR, 0x01, 1); //EN pipe #0
     
     //RF_Address width setup (setup here as 5)
-    write_read_byte_nrf_SPI(WRITE_BIT,SETUP_AW,0b00000011,1);
+    rw_nrf(WRITE_BIT,SETUP_AW,0b00000011,1);
     
     //RF channel setup, choose freq, 2.4 - 2.527 GHz 1MHz/step
     //setup as 120
-    write_read_byte_nrf_SPI(WRITE_BIT,RF_CH,120,1);
+    rw_nrf(WRITE_BIT,RF_CH,120,1);
     
     //RF setup - choose pwr and data rate 
     //setup here for 250kbps and 0dBm pwr
-    write_read_byte_nrf_SPI(WRITE_BIT,RF_SETUP, ((1<<RF_DR_LOW)|0x03),1);
+    rw_nrf(WRITE_BIT,RF_SETUP, ((1<<RF_DR_LOW)|0x03),1);
     
     //RX RF_Address setup 
     memcpy(val, "\xDE\xAD\xBE\xEF\x00", 5);
-    write_read_byte_nrf_SPI(WRITE_BIT,RX_ADDR_P0,val,5); //pipe #0 address
+    rw_nrf(WRITE_BIT,RX_ADDR_P0,val,5); //pipe #0 address
     //set TX Address same in this setup.
-    write_read_byte_nrf_SPI(WRITE_BIT,TX_ADDR, val,5);
+    rw_nrf(WRITE_BIT,TX_ADDR, val,5);
     
     
     //Payload width
@@ -143,13 +147,13 @@ void init_nrf(void){
     
     //set for dynamic payload allocation/width whatever, stuff
     //enable it on pipe #0
-    write_read_byte_nrf_SPI(WRITE_BIT, DYNPD, (1<<DPL_P0), 1);
+    rw_nrf(WRITE_BIT, DYNPD, (1<<DPL_P0), 1);
     //and enable it globally
-    write_read_byte_nrf_SPI(WRITE_BIT, FEATURE, (1<<EN_DPL),1);
+    rw_nrf(WRITE_BIT, FEATURE, (1<<EN_DPL),1);
     
     //CONFIG reg setup = boot nrf and choose rx or tx
     //set as tx, pwr up, and irq not triggered by transmittion failure
-    write_read_byte_nrf_SPI(WRITE_BIT, CONFIG, ((1<<MASK_MAX_RT)|
+    rw_nrf(WRITE_BIT, CONFIG, ((1<<MASK_MAX_RT)|
                                                 (1<<EN_CRC) |
                                                 (1<<PWR_UP)), 1);
                                                 
@@ -159,22 +163,63 @@ void init_nrf(void){
 }
 void transmit_nrf_payload(uint8_t * W_buff){
     
+    rw_nrf(READ_BIT, FLUSH_TX, W_buff,0);//sends 0xe1 to flush registry
+    rw_nrf(READ_BIT, W_TX_PAYLOAD, W_buff, sizeof(W_buff));//sends data to nrf
+    //using READ b/c they are on hightest byte level.
     
+    #if USING_INT0_IRQ==1
+    sei(); //enable global interrupts
+    #endif
     
+    _delay_ms(10); //need 10ms delay for some reason
+    RF_CTRL_PORT |= (RF_CE); //transmit the data, set CE high to enable
+    _delay_us(20); //delay at least 10us!
+    RF_CTRL_PORT &= ~(RF_CE);//CE low, stop transmitting
+    _delay_ms(10); //delay before proceeding
 }
 void receive_nrf_payload(void){
+    //for when in reciever mode
+    //should be made more efficient
     
+    #if USING_INT0_IRQ==1
+    sei(); //enable global interrupts
+    #endif
     
+    RF_CTRL_PORT |= RF_CE; //make it listen for data
+    _delay_ms(1000); //listen 1s at a time 
+    RF_CTRL_PORT &= ~(RF_CE); //CE low, stop listening
+    
+    #if USING_INT0_IRQ==1
+    cli(); //disable global interrupts
+    #endif
     
 }
 void reset_nrf(void){
     
-    
+    _delay_us(10);
+    RF_CTRL_PORT |= (RF_CSN); //CSN low
+    _delay_us(10);
+    write_read_byte_nrf_SPI(W_REGISTER + STATUS); //write status to registry
+    _delay_us(10);
+    write_read_byte_nrf_SPI(0x70); //reset all irqs in STATUS registry
+    _delay_us(10);
+    RF_CTRL_PORT |= RF_CSN; //CSN IR_High
     
 }
 
 void init_nrf_INT0_IRQ(void){
     
-    
+    #ifdef USING_ATTINY26
+        
+        DDRB &= ~(1<<DDB6); //external interrupt int0, make sure it's input
+        
+        MCUCR |= (1<<ISC01); //into falling edge pb6
+        MCUCR &= ~(1<<ISC00); //falling edge stuff
+        
+        GIMSK |= (1<<INT0); //enable INT0 interrupt
+        
+        //sei(); //enable global interrupts, should do later
+        
+    #endif
     
 }
