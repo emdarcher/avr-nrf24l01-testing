603bf4c5417717f1cedb26c2dfcfd03fcba668dd
emdarcher <emd519@yahoo.com>
2014-08-08 18:08:22 -0700

    starting some code, but sadly I think my attiny26 is bricked! :(

---
+++ basic_comm/tx_testing/main.c
@@ -0,0 +1,22 @@
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdint.h>
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>
+#include <avr/pgmspace.h>
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+#include <avr/power.h>
+
+#include "my_nrf.h"
+
+
+
+int main(void)
+{
+    
+    
+    
+}
---
+++ basic_comm/tx_testing/my_nrf.h
@@ -0,0 +1,38 @@
+//lib based on parts of this blog post:
+// http://gizmosnack.blogspot.com/2013/04/tutorial-nrf24l01-and-avr.html
+
+
+#ifndef MY_NRF_H
+#define MY_NRF_H
+
+#define RF_CTRL_DDR
+#define RF_CTRL_PORT
+#define RF_CSN  (1<<4)
+#define RF_CE   (1<<5)
+#define RF_IRQ  (1<<6)
+
+#define W 1
+#define R 0
+
+#define USING_ATTINY26
+//#define USING_ATTINYx5
+//#define USING_ATMEGAxx8
+
+#define USING_INT0_IRQ 1
+
+//prototypes
+
+void init_nrf_SPI(void);
+void write_byte_nrf_SPI(uint8_t cData);
+uint8_t get_nrf_reg(uint8_t reg);
+void write_to_nrf(uint8_t reg, uint8_t Package);
+uint8_t *rw_nrf(uint8_t ReadWrite, uint8_t reg, uint8_t *val, uint8_t antVal);
+void init_nrf(void);
+void transmit_nrf_payload(uint8_t * W_buff);
+void receive_nrf_payload(void);
+void reset_nrf(void);
+
+void init_nrf_INT0_IRQ(void);
+
+
+#endif
